9072473ed686fd582ec30b578b0ff6eaa780b83f
yecguanchuy <89492553+yecguanchuy@users.noreply.github.com>
2022-10-07 21:41:00 -0500

    Delete CrudUsc directory

--- CrudUsc/src/main/java/com/usc/crud/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.usc.crud.repository;
-
-import com.usc.crud.model.Empleado;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<Empleado, Long> {
-}
